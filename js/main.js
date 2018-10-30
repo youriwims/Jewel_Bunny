b00dce23d9ff7b6282d17b6ddcf5011c897ae744
Youri <ywims1@gmail.com>
2018-10-30 13:49:48 -0400

    fixed js src injection

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var imgArray = new Array();
+var placeHolderArray = new Array();
+
+
+for (var i = 1; i < 13; i++) {
+    imgArray.push(`jewel${i}.png`);
+    placeHolderArray.push(`#myImage${i}`);
+}
+
+
+var imgArray = new Array();
+var objectUrl;
+
+for (var i = 0; i < 12; i++) {
+    console.log(placeHolderArray[i]);
+    document.querySelector(placeHolderArray[i]).src = `https://youriwims.github.io/img/jewel${i}.png`;
+}
